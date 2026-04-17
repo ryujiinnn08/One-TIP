a234afe747ba1e4f6d9fa46500712f74ff30efee
Francis Ruzgal <francisaaronruzgal@gmail.com>
2026-04-17 15:10:30 +0800

    Fix: Remove [assets] from wrangler.toml to enable Pages Functions, fix CSP for Icons8, move _headers to root

--- .wrangler/tmp/dev-SyBchi/functionsWorker-0.6780345835783207.js
+++ .wrangler/tmp/dev-SyBchi/functionsWorker-0.6780345835783207.js
@@ -1095,7 +1095,7 @@ var jsonError2 = /* @__PURE__ */ __name(async (request, env, _ctx, middlewareCtx
 }, "jsonError");
 var middleware_miniflare3_json_error_default2 = jsonError2;
 
-// .wrangler/tmp/bundle-m0hpYP/middleware-insertion-facade.js
+// .wrangler/tmp/bundle-WEQv2v/middleware-insertion-facade.js
 var __INTERNAL_WRANGLER_MIDDLEWARE__2 = [
   middleware_ensure_req_body_drained_default2,
   middleware_miniflare3_json_error_default2
@@ -1127,7 +1127,7 @@ function __facade_invoke__2(request, env, ctx, dispatch, finalMiddleware) {
 }
 __name(__facade_invoke__2, "__facade_invoke__");
 
-// .wrangler/tmp/bundle-m0hpYP/middleware-loader.entry.ts
+// .wrangler/tmp/bundle-WEQv2v/middleware-loader.entry.ts
 var __Facade_ScheduledController__2 = class ___Facade_ScheduledController__2 {
   constructor(scheduledTime, cron, noRetry) {
     this.scheduledTime = scheduledTime;
